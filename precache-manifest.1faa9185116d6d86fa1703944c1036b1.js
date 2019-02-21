c8a7ab3e0e518607dd775fd30cadf9e5e12a5f32
Durga Sankar Pasupuleti <durga.pasupuleti@corp.ads.valuelabs.net>
2019-02-21 14:59:26 +0530

    Updates

---
+++ precache-manifest.1faa9185116d6d86fa1703944c1036b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6aaef7d3fc7228d13319",
+    "url": "/BusApp/static/js/runtime~main.6aaef7d3.js"
+  },
+  {
+    "revision": "2c7c6e0f7c9aee86f864",
+    "url": "/BusApp/static/js/main.2c7c6e0f.chunk.js"
+  },
+  {
+    "revision": "8959c84b9b50a1021885",
+    "url": "/BusApp/static/js/2.8959c84b.chunk.js"
+  },
+  {
+    "revision": "2c7c6e0f7c9aee86f864",
+    "url": "/BusApp/static/css/main.6acb4773.chunk.css"
+  },
+  {
+    "revision": "e6973db326cb9d4f3cec916cc67bdfd9",
+    "url": "/BusApp/index.html"
+  }
+];
